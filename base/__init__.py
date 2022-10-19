cc2ec3bccba43a1e6732a0b91ff3b60f39ded6f5
Felix Pan <tinylambda@gmail.com>
2022-10-19 11:41:39 +0800

    add formatter for log

--- base/__init__.py
+++ base/__init__.py
@@ -5,4 +5,8 @@ keep_logger = logging.getLogger("KeepLogger")
 keep_logger.setLevel(logging.INFO)
 
 logger_handler_stderr = logging.StreamHandler(stream=sys.stderr)
+logger_formatter = logging.Formatter(
+    fmt="{asctime}\t{levelname}\t{message!r}", style="{"
+)
+logger_handler_stderr.setFormatter(logger_formatter)
 keep_logger.addHandler(logger_handler_stderr)
